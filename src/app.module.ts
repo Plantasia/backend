17e6fd2f9bc7c9804c4e220033965d37fbe9d68f
Matheus Faggi <matheusfaggi@gmail.com>
2020-11-11 09:24:11 -0300

    remove others dependecies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoryModule } from './category/module/category.module';
-import { Connection } from 'typeorm';
+
 import { TopicsController } from './topics/controllers/topics.controller';
 import { CategoryService } from './category/service/category.service';
 import { UserController } from './user/user.controller';
 import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(), CategoryModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [TopicsController, UserController],
   providers: [CategoryService],
 })
 export class AppModule {
-  constructor(private connection: Connection) {}
+  constructor() {}
 }
