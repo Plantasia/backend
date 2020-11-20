77d4acd07642dd9e332143c4ac26267f58c4a5a2
marcosv23 <marcos_v.23@hotmail.com>
2020-11-20 11:59:09 -0300

    * testing and adjusting routes

---
+++ build/migration/1605792274701-create-topic.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createTopic1605792274701 = void 0;
+class createTopic1605792274701 {
+    async up(queryRunner) {
+    }
+    async down(queryRunner) {
+    }
+}
+exports.createTopic1605792274701 = createTopic1605792274701;
+//# sourceMappingURL=1605792274701-create-topic.js.map
