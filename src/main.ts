3cb315e5bb8ef86c6d07724e8fb00980aa0f5c1b
Matheus Faggi <matheusfaggi@gmail.com>
2021-01-08 02:49:45 -0300

    small changes adn add enviroment variable

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { resolve } from 'path';
 import { AppModule } from './app.module';
 
+async function bootstrap() {
+  const PORT = process.env.PORT || 3333;
+  const HOST = process.env.HOST || '0.0.0.0';
 
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-    console.log(`Application is running on: ${await app.getUrl()}`);
-  }
-  bootstrap();
+  const app = await NestFactory.create(AppModule);
 
-
-
+  await app.listen(PORT, HOST);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
