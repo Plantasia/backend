93512c12dc33bc480288ccecf3f87613fddb7f7b
Matheus Faggi <matheusfaggi@gmail.com>
2021-01-11 02:14:21 -0300

    fix: typeorm path

--- src/config/cli-typeorm.js
+++ src/config/cli-typeorm.js
@@ -14,10 +14,10 @@ module.exports = {
   synchronize: true,
   port: 3306,
   logging: true,
-  entities: [path.resolve(__dirname, 'src', 'database', 'models', '*')],
+  entities: [path.resolve(__dirname, 'src', 'entities', '*')],
   migrations: [path.resolve(__dirname, '..', 'database', 'migrations', '*')],
   cli: {
-    entitiesDir: path.resolve(__dirname, 'src', 'database', 'models'),
+    entitiesDir: path.resolve(__dirname, 'src', 'entities'),
     migrationsDir: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
 };
