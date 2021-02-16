bab224e3e5a266b99736ac065c6305681b9a4239
juanCsrl <cattjuan@gmail.com>
2021-02-16 11:23:12 -0300

    add adminBro

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,13 @@ import { UserModule } from './modules/profile/user/user.module';
 import { ForumModule } from './modules/forum/forum.module';
 import {AuthService} from './/auth/auth.service'
 import {  AppService } from './app.service';
+import { AdminModule } from '@admin-bro/nestjs';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({}), UserModule, AuthModule, ForumModule, AppService],
+  imports: [TypeOrmModule.forRoot({}), UserModule, AuthModule, ForumModule, AppService,AdminModule.createAdmin({
+    adminBroOptions: {
+         rootPath: '/admin',
+         resources: [],
+ }})],
 })
 export class AppModule {}
