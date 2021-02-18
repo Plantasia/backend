765a22239abdf3e8b9b3fd86f70392258ce7da5c
juanCsrl <cattjuan@gmail.com>
2021-02-17 23:47:32 -0300

    remove adminBro

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,29 +5,13 @@ import { UserModule } from './modules/profile/user/user.module';
 import { ForumModule } from './modules/forum/forum.module';
 import {AuthService} from './/auth/auth.service'
 import {  AppService } from './app.service';
-import AdminBro from 'admin-bro';
-import { AdminModule } from '@admin-bro/nestjs';
-import { Database, Resource } from '@admin-bro/typeorm'
-import {User} from '@entities/user.entity'
 
-AdminBro.registerAdapter({ Database, Resource })
 
 
 
 
 
 @Module({
-  imports: [UserModule, AuthModule, ForumModule, AppService,TypeOrmModule.forRoot({}),
-    AdminModule.createAdmin({
-        adminBroOptions: {
-            rootPath: '/admin',
-            resources: [User],
-          },
-          auth: {
-            authenticate: async (email, password) => Promise.resolve({ email: 'test' }),
-            cookieName: 'test',
-            cookiePassword: 'testPass',
-          },
-    }),],
+  imports: [UserModule, AuthModule, ForumModule, AppService,TypeOrmModule.forRoot({})],
 })
 export class AppModule {}
