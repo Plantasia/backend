e4d3d611845a1e434074fca29563ff061a679474
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-05 03:38:20 -0300

    lint

--- src/modules/image/imageS3.service.ts
+++ src/modules/image/imageS3.service.ts
@@ -1,23 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
- 
+
 @Injectable()
 export class FilesService {
-  constructor(
+  constructor() {}
 
-  ) {}
- 
-  async uploadPublicFile(dataBuffer: Buffer, filename: string) {
+  async uploadPublicFile(
+    dataBuffer: Buffer,
+    filename: string,
+  ): Promise<string> {
     const AWS_S3_BUCKET_NAME = process.env.AWS_PUBLIC_BUCKET_NAME;
     const s3 = new S3();
-    const uploadResult = await s3.upload({
-      Bucket: AWS_S3_BUCKET_NAME,
-      Body: dataBuffer,
-      Key: `${uuid()}-${filename}`
-    })
+    const uploadResult = await s3
+      .upload({
+        Bucket: AWS_S3_BUCKET_NAME,
+        Body: dataBuffer,
+        Key: `${uuid()}-${filename}`,
+      })
       .promise();
- 
+
     return uploadResult.Location;
   }
 }
