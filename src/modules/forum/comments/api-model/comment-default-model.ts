f8481eee7b24b8827bb9c8124077caa9d226ffec
marcos <marcos_v.23@hotmail.com>
2021-06-06 17:40:53 -0300

    *fix: adjusting find one comment to return both userId and topicId

--- src/modules/forum/comments/api-model/comment-default-model.ts
+++ src/modules/forum/comments/api-model/comment-default-model.ts
@@ -1,7 +1,10 @@
 export default class CommentModel{
     id:string;
     textBody:string;
-    updated_at:string;
-    created_at:string;
-    deleted_at:string;
+    updated_at:string | Date;
+    created_at:string | Date;
+    deleted_at: string | Date;
+    userId: string;
+    topicId: string;
+    
 }
