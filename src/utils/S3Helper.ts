353d4f75fe63aeab601fa586a460f5a741d0ea02
Matheus Faggi <matheusfaggi@gmail.com>
2021-06-07 23:15:08 -0300

    fix: set s3 region by environment variable

--- src/utils/S3Helper.ts
+++ src/utils/S3Helper.ts
@@ -1,12 +1,18 @@
 import { S3 } from 'aws-sdk';
 export class S3Helper {
-  AWS_S3_BUCKET_NAME = process.env.AWS_PUBLIC_BUCKET_NAME;
-  client: S3 = new S3();
+  AWS_PUBLIC_BUCKET_NAME: string;
+  client: S3;
+  constructor() {
+    const { AWS_PUBLIC_BUCKET_NAME, AWS_REGION } = process.env;
+    this.AWS_PUBLIC_BUCKET_NAME = AWS_PUBLIC_BUCKET_NAME;
+
+    this.client = new S3({ region: AWS_REGION });
+  }
 
   async upload(path: string, buffer: Buffer): Promise<void> {
     await this.client
       .upload({
-        Bucket: this.AWS_S3_BUCKET_NAME,
+        Bucket: this.AWS_PUBLIC_BUCKET_NAME,
         Body: buffer,
         Key: path,
       })
@@ -14,7 +20,7 @@ export class S3Helper {
   }
   async getUrl(path: string, seconds: number = 60 * 60 * 24): Promise<string> {
     return this.client.getSignedUrl('getObject', {
-      Bucket: this.AWS_S3_BUCKET_NAME,
+      Bucket: this.AWS_PUBLIC_BUCKET_NAME,
       Key: path,
       Expires: seconds,
     });
